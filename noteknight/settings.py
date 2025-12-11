0b48a0d3be30a35353c93e2a0ec7455f1d2088d6
RaunTheIronshod <robinstonechild@gmail.com>
2025-12-11 20:01:18 +0000

    Fix: widget tweaks added for django for login.html

--- noteknight/settings.py
+++ noteknight/settings.py
@@ -35,9 +35,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'widget_tweaks'
+    'widget_tweaks',
 
-    # My apps
+    # Your apps
     'notes',
 ]
 
